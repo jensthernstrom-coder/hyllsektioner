06d9003669a96e23715911f1712a6eded1ca8019
jensthernstrom-coder <jens.thernstrom@gmail.com>
2026-09-23 14:48:40 +0200

    Split legacy shelf values on spaced slash

--- extensions/butikshylla-block/src/BlockExtension.jsx
+++ extensions/butikshylla-block/src/BlockExtension.jsx
@@ -216,7 +216,7 @@ function parseLegacyReadableShelf(value) {
 
   return uniqueShelves(
     String(value)
-      .split(/[,;\n]/)
+      .split(/[,;\n]|\s+\/\s+/)
       .map((part) => part.trim()),
   );
 }
